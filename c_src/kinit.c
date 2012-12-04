c2a9a21c6f7ee82b73bddf7f8a435ba036ecbb63
Simon Lundström <simlu@su.se>
2012-12-04 16:11:10 +0100

    Add the real kinit.c
    Lots of fixing to do though, but atleast it "works". Only works on
    Heimdal for now.

--- c_src/kinit.c
+++ c_src/kinit.c
@@ -1,3 +1,56 @@
-int main(int argc, char **argv) {
-	return 0;
+#include <err.h>
+#include <krb5.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  krb5_error_code error;
+  krb5_principal  principal;
+  krb5_context    context;
+  krb5_creds      creds;
+
+  char *user = argv[1];
+  char password[1024];
+  char *password_ptr = password;
+
+  if (argc < 2) {
+    errx(1, "Username is missing as first argument");
+  }
+
+  // Initialize krb5 context
+  if ((error = krb5_init_context(&context))) {
+    return error;
+  }
+
+  memset(&creds, 0, sizeof(krb5_creds));
+
+  // Create the principal name from user and /etc/krb5.conf
+  if ((error = krb5_parse_name(context, user, &principal))) {
+    goto free_context;
+  }
+
+  // Read password from STDIN
+  if ((NULL == fgets(password, sizeof(password), stdin))) {
+    // FIXME goto free_context? Why not?
+    errx(1, "Got NULL from fgets when reading password");
+  }
+
+  // Remove trailing newline
+  strsep(&password_ptr, "\n");
+
+  // Do kinit
+  if ((error = krb5_get_init_creds_password(
+          context, &creds, principal, password, 0, NULL, 0, NULL, NULL))) {
+    krb5_err(context, 1, error, "krb5_get_init_creds_password");
+  }
+
+  krb5_free_cred_contents(context, &creds);
+  krb5_free_principal(context, principal);
+  // TODO Erase password
+
+free_context:
+  krb5_free_context(context);
+  printf("error: %i\n", error);
+  return(error);
 }
