0bcab9dae52e680b345f83228a83370f370fc929
Simon Lundström <simlu@su.se>
2012-12-12 10:27:45 +0100

    Add a noop for upgrade so we don't get the warning.

--- c_src/kinit.c
+++ c_src/kinit.c
@@ -1,12 +1,15 @@
 #include <erl_nif.h>
 
-static ERL_NIF_TERM kinit(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
-{
+static ERL_NIF_TERM kinit(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]) {
     return enif_make_atom(env, "true");
+}
+
+int noop(ErlNifEnv* env, void** priv_data, void** old_priv_data, ERL_NIF_TERM load_info) {
+    return 0;
 }
 
 static ErlNifFunc nif_funcs[] = {
     {"kinit", 2, kinit}
 };
 
-ERL_NIF_INIT(kinit, nif_funcs, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(kinit, nif_funcs, NULL, NULL, noop, NULL)
