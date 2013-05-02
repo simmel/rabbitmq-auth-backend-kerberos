68daa16e5a11ee2a810f86963676baa1ef4cbc32
Simon Lundström <simlu@su.se>
2013-05-02 12:47:29 +0200

    After discussions, time skew should be an error

--- c_src/kinit.c
+++ c_src/kinit.c
@@ -56,8 +56,7 @@ ERL_NIF_TERM error_and_exit(ErlNifEnv* env, struct kebab *kebab, char *tag) {
   // FIXME This should be done in a better way
   else if (kebab->error == KRB5KRB_AP_ERR_BAD_INTEGRITY ||
            kebab->error == KRB5KDC_ERR_PREAUTH_FAILED ||
-           kebab->error == KRB5KDC_ERR_KEY_EXPIRED ||
-           kebab->error == KRB5KRB_AP_ERR_SKEW
+           kebab->error == KRB5KDC_ERR_KEY_EXPIRED
           )
      return_atom = enif_make_atom(env, "refused");
   else
